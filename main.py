8d63e16b720a3f94c1503e9807686894ccc3b8f2
yoyobuzz <lakshitsethia@gmail.com>
2024-08-28 11:18:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,9 @@
 # def read_root():
 #     return {"###": "Jai Shree Ram!"}
 
+from app.main import app
+
 if __name__ == '__main__':
     import uvicorn
     # uvicorn.run(app)
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app", host="0.0.0.0", port=8000, reload=True)
