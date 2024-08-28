ae44b1830a6fb0d94b2f727548c72523720e6acf
yoyobuzz <lakshitsethia@gmail.com>
2024-08-29 00:32:59 +0530

    Added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, albums, face_recog
 from app.core.database import Base, engine
 
@@ -6,6 +7,15 @@ from app.core.database import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://localhost:3000"],  # Allow requests from localhost or React dev server
+    allow_credentials=True,  # Allow cookies to be sent in cross-origin requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers, including Authorization
+)
 
 # Include the routes
 app.include_router(auth.router, prefix="", tags=["auth"])
